a571509ed041858994b24a47e44f0b82de21e819
Anandhu97461 <anandhu.offc@gmail.com>
2025-06-28 13:46:46 +0530

    week-5 first script added

---
+++ week-5/first-script/create-directory-structure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /home/bob/countries/{USA,UK,India}
+echo "Washington, D.C" > /home/bob/countries/USA/capital.txt
+echo "London" > /home/bob/countries/UK/capital.txt
+echo "New Delhi" > /home/bob/countries/India/capital.txt
+uptime
